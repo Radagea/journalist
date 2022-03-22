6a94d5fc3a9d6a7de1374436eb4c22f1fd02e960
Radagea <radagea@gmail.com>
2022-03-22 20:26:28 +0000

    Article page -> ArticleTitle section finished

---
+++ src/components/UI/cards/articles/ArticleTitle.vue
@@ -0,0 +1,165 @@
+<template>    
+    <section>
+        <div class="arrow-down"></div>
+        <div class="wrapper">
+            <div class="articletitle">
+                <h1>Walking the line: balancing performance barriers and facilitators in an augmented reality mobile application for paediatric code cart training</h1>
+                <h2>
+                    <span>Abigail R. Wooldridge</span>
+                    <span>Widya A. Ramadhani</span>
+                    <span>Keith Hanson</span>
+                    <span>Elsa Vazquez-Melendez</span>
+                </h2>
+                <h3>
+                    <span>Augmented reality</span>
+                    <span>paediatric resuscitation</span>
+                    <span>code carts</span>
+                    <span>sociotechnical system design</span>
+                    <span>work system analysis</span>
+                </h3>
+                <nav>
+                    <ul>
+                        <li class="active">Abstract</li>
+                        <li>Full text</li>
+                        <li>Get access</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="articledatas">
+                <h1>230</h1>
+                <p>Views</p>
+                <h1>2019.12.11</h1>
+                <p>Published date</p>
+                <h1>2019.11.01</h1>
+                <p>Recived date</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    section {
+        width: 100%;
+        overflow: hidden;
+        background-color: #B67E86;
+    }
+    div.arrow-down {
+        margin-left: 30%;
+        margin-right: auto;
+        width: 0;
+        height: 0;
+        border-left: 20px solid transparent;
+        border-right: 20px solid transparent;
+        border-top: 20px solid #783741;
+    }
+    .wrapper {
+        margin: 0 auto;
+        width: 90%;
+        max-width: 1400px;
+        color: white;
+    }
+    .articletitle {
+        float: left;
+        width: 80%;
+        margin-right: 1%;
+    }
+    .articletitle h1 {
+        font-size: 1.8em;
+        margin-bottom: 10px;
+    }
+    .articletitle h2 {
+        color: #e0e0e0;
+        font-size: 16px;
+        margin-bottom: 40px;
+    }
+    .articletitle h2 span {
+        cursor: pointer;
+        transition: 0.2s;
+        margin-right: 15px;
+    }
+    .articletitle h2 span:hover {
+        color: #7F444D;
+    }
+
+    .articletitle h3 {
+        color: white;
+        font-size: 14px;
+    }
+
+    .articletitle h3 span {
+        display: inline-block;
+        cursor: pointer;
+        padding: 5px;
+        background-color: #783741;
+        border-radius: 8px;
+        transition: 0.2s;
+        margin-right: 15px;
+    }
+
+    .articletitle h3 span:hover {
+        color: #e0e0e0;
+    }
+
+    @media(max-width:900px) {
+        .articletitle h1 {
+            font-size: 1.5em;
+        }
+        .articletitle h2 {
+            font-size: 12px;
+        }
+    }
+    .articledatas {
+        float: left;
+        text-align: right;
+        width: 18%;
+    }
+    .articledatas p {
+        padding-bottom: 5px;
+        border-bottom: 1px solid grey;
+    }
+
+    @media(max-width: 500px) {
+        .articledatas {
+            display: none;
+            width: 0%;
+        }
+        .articletitle {
+            width: 100%;
+            margin-right: 0;
+        }
+    }
+    nav {
+        margin-top: 60px;
+    }
+    ul {
+        margin: 0 auto;
+        text-align: center;
+        list-style: none;
+    }
+    ul li {
+        display: inline-block;
+        cursor: pointer;
+        background-color: #783741;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        padding-right: 5%;
+        padding-left: 5%;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        font-size: 1.2em;
+        transition: 0.3s;
+    }
+    ul li:hover {
+        background-color: #9b6c73;
+    }
+    ul li.active {
+        background-color: white;
+        color: #783741;
+    }
+</style>
--- src/components/pages/MainArticlePage.vue
+++ src/components/pages/MainArticlePage.vue
@@ -1,112 +1,24 @@
 <template>
     <journal-in-article></journal-in-article>
-    <section>
-        <div class="arrow-down"></div>
-        <div class="wrapper">
-            <div class="articletitle">
-                <h1>Walking the line: balancing performance barriers and facilitators in an augmented reality mobile application for paediatric code cart training</h1>
-                <h2>
-                    <span>Abigail R. Wooldridge</span>
-                    <span>Widya A. Ramadhani</span>
-                    <span>Keith Hanson</span>
-                    <span>Elsa Vazquez-Melendez</span>
-                </h2>
-            </div>
-            <div class="articledatas">
-                <h1>230</h1>
-                <p>Views</p>
-                <h1>2019.12.11</h1>
-                <p>Published date</p>
-                <h1>2019.11.01</h1>
-                <p>Recived date</p>
-            </div>
-        </div>
-    </section>
+    <article-title></article-title>
+    <main></main>
 </template>
 
 <script>
 import JournalInArticle from '../UI/cards/articles/JournalInArticle.vue';
+import ArticleTitle from '../UI/cards/articles/ArticleTitle.vue';
 export default {
     components: {
         JournalInArticle,
-
+        ArticleTitle
     }
 }
 </script>
 
 <style scoped>
-    section {
+    main {
         width: 100%;
-        min-height: 20rem;
-        overflow: hidden;
-        background-color: #B67E86;
-    }
-    div.arrow-down {
-        margin-left: 30%;
-        margin-right: auto;
-        width: 0;
-        height: 0;
-        border-left: 20px solid transparent;
-        border-right: 20px solid transparent;
-        border-top: 20px solid #783741;
-    }
-    .wrapper {
-        margin: 0 auto;
-        width: 90%;
-        max-width: 1400px;
-        color: white;
-    }
-    .articletitle {
-        float: left;
-        width: 80%;
-        margin-right: 1%;
-    }
-    .articletitle h1 {
-        font-size: 1.8em;
-        margin-bottom: 10px;
-    }
-    .articletitle h2 {
-        color: #e0e0e0;
-        font-size: 16px;
-    }
-    .articletitle h2 span {
-        cursor: pointer;
-        transition: 0.2s;
-        margin-right: 15px;
-    }
-    .articletitle h2 span:hover {
-        color: #7F444D;
-    }
-
-    @media(max-width:900px) {
-        .articletitle h1 {
-            font-size: 1.5em;
-        }
-        .articletitle h2 {
-            font-size: 12px;
-        }
-    }
-    .articledatas {
-        float: left;
-        text-align: right;
-        width: 18%;
-    }
-    .articledatas p {
-        padding-bottom: 5px;
-        border-bottom: 1px solid grey;
-    }
-    .articledatas h1 {
-        margin-top: 20px;
-    }
-
-    @media(max-width: 500px) {
-        .articledatas {
-            display: none;
-            width: 0%;
-        }
-        .articletitle {
-            width: 100%;
-            margin-right: 0;
-        }
+        height: 50rem;
+        background-color: white;
     }
 </style>
