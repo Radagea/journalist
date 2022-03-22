22d84f43740cee8fea90b956743e94f3079eec1a
Radagea <radagea@gmail.com>
2022-03-22 19:27:38 +0000

    Article page started again

---
+++ src/components/UI/cards/articles/JournalInArticle.vue
@@ -0,0 +1,143 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <div class="journal">
+                <img src="/images/nophoto.jpg" alt="Journal main Picture">
+                <h1>Acta Cardiologica</h1>
+                <p>Articles number: 231</p>
+                <a href="#">Journal page</a>
+            </div>
+            <div class="searcher">
+                <div class="wrap">
+                    <input class="mainsearch" type="text" id="mainsearch" name="mainsearch" placeholder="Start Searching..." v-model="mainSearch">
+                    <button class="searchButton">Search</button>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mainSearch: '',
+        };
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        width: 100%;
+        height: 10rem;
+        background-color: #783741;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+    .wrapper {
+        width: 90%;
+        max-width: 1400px;
+        height: 100%;
+        margin: 0 auto;
+    }
+    .journal {
+        float: left;
+        height: 100%;
+        width: 40%;
+        position: relative;
+    }
+    .journal img {
+        float: left;
+        height: 100%;
+        margin-right: 10px;
+    }
+    .journal h1 {
+        color: white;
+        font-size: 1.5em;
+    }
+    .journal p {
+        color: white;
+    }
+    .journal a {
+        position: absolute;
+        text-decoration: none;
+        padding: 10px;
+        background-color: #b1626d;
+        color: white;
+        bottom: 0;
+        border-radius: 10px;
+        transition: 0.2s;
+    }
+    .journal a:hover {
+        background-color: #96646b;
+    }
+
+    .searcher {
+        position: relative;
+        float: left;
+        width: 60%;
+        height: 100%;
+    }
+    .wrap {
+        margin-top: 3rem;
+    }
+    input.mainsearch {
+        float: left;
+        width: 88%;
+        height: 3rem;
+        padding-left: 1%;
+        margin-right: 1%;
+        transition: 0.2s;
+    }
+    button {
+        color: white;
+        cursor: pointer;
+        width: 10%;
+        height: 3rem;
+        background-color: #b1626d;
+        border-radius: 10px;
+        transition: 0.2s;
+    }
+    button:hover {
+        background-color: #96646b;
+    }
+    input:focus,
+    input:hover {
+        outline: 3px solid #ddd;
+    }
+    @media(max-width:790px) {
+        .searcher {
+            width: 50%;
+        }
+        h1 {
+            font-size: 1.4em;
+        }
+        input.mainsearch {
+            width: 78%;
+        }
+        button {
+            width: 20%;
+        }
+        .journal {
+            width: 50%;
+        }
+    }
+    @media(max-width: 580px) {
+        .searcher {
+            width: 40%;
+        }
+        .journal {
+            width: 60%;
+        }
+    }
+    @media(max-width: 500px) {
+        .searcher {
+            width: 0%;
+            display: none;
+        }
+        .journal {
+            width: 100%;
+        }
+    }
+</style>
--- src/components/pages/MainArticlePage.vue
+++ src/components/pages/MainArticlePage.vue
@@ -1,47 +1,112 @@
 <template>
-    <header>
+    <journal-in-article></journal-in-article>
+    <section>
+        <div class="arrow-down"></div>
         <div class="wrapper">
-            <div class="journal">
-                <img src="/images/nophoto.jpg" alt="Journal main Picture">
-                <h1>Acta Cardiologica</h1>
+            <div class="articletitle">
+                <h1>Walking the line: balancing performance barriers and facilitators in an augmented reality mobile application for paediatric code cart training</h1>
+                <h2>
+                    <span>Abigail R. Wooldridge</span>
+                    <span>Widya A. Ramadhani</span>
+                    <span>Keith Hanson</span>
+                    <span>Elsa Vazquez-Melendez</span>
+                </h2>
+            </div>
+            <div class="articledatas">
+                <h1>230</h1>
+                <p>Views</p>
+                <h1>2019.12.11</h1>
+                <p>Published date</p>
+                <h1>2019.11.01</h1>
+                <p>Recived date</p>
             </div>
         </div>
-    </header>
-    <section>
     </section>
 </template>
 
 <script>
+import JournalInArticle from '../UI/cards/articles/JournalInArticle.vue';
 export default {
+    components: {
+        JournalInArticle,
 
+    }
 }
 </script>
 
 <style scoped>
-    header {
+    section {
         width: 100%;
-        height: 10rem;
-        background-color: #783741;
-        padding-top: 10px;
-        padding-bottom: 10px;
-        margin-bottom: 10px;
+        min-height: 20rem;
+        overflow: hidden;
+        background-color: #B67E86;
+    }
+    div.arrow-down {
+        margin-left: 30%;
+        margin-right: auto;
+        width: 0;
+        height: 0;
+        border-left: 20px solid transparent;
+        border-right: 20px solid transparent;
+        border-top: 20px solid #783741;
     }
     .wrapper {
+        margin: 0 auto;
         width: 90%;
         max-width: 1400px;
-        height: 100%;
-        margin: 0 auto;
-    }
-    .journal {
-        height: 100%;
-    }
-    .journal img {
-        float: left;
-        height: 100%;
-        margin-right: 10px;
-    }
-    .journal h1 {
         color: white;
-        font-size: 1.5em;
+    }
+    .articletitle {
+        float: left;
+        width: 80%;
+        margin-right: 1%;
+    }
+    .articletitle h1 {
+        font-size: 1.8em;
+        margin-bottom: 10px;
+    }
+    .articletitle h2 {
+        color: #e0e0e0;
+        font-size: 16px;
+    }
+    .articletitle h2 span {
+        cursor: pointer;
+        transition: 0.2s;
+        margin-right: 15px;
+    }
+    .articletitle h2 span:hover {
+        color: #7F444D;
+    }
+
+    @media(max-width:900px) {
+        .articletitle h1 {
+            font-size: 1.5em;
+        }
+        .articletitle h2 {
+            font-size: 12px;
+        }
+    }
+    .articledatas {
+        float: left;
+        text-align: right;
+        width: 18%;
+    }
+    .articledatas p {
+        padding-bottom: 5px;
+        border-bottom: 1px solid grey;
+    }
+    .articledatas h1 {
+        margin-top: 20px;
+    }
+
+    @media(max-width: 500px) {
+        .articledatas {
+            display: none;
+            width: 0%;
+        }
+        .articletitle {
+            width: 100%;
+            margin-right: 0;
+        }
     }
 </style>
